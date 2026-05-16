5764627db68560e68594683273406654f714723d
fakbayrak <farukakbayrak0@gmail.com>
2026-05-17 00:01:59 +0300

    Add GameManager.cs - Core script added

---
+++ LastColony/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
